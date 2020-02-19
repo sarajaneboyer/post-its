43c3934c1961523158fee1e52179b63a24e786a6
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-18 17:05:29 -0700

    created _navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_notes#home' #tells the app where to go when you first launch the site and static_notes is the controller #home is the method
+
+  get '/about', to: 'static_notes#about'
+
   resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
