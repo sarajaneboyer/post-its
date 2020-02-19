5b22a18153de39a9ba714be56394b588acabf291
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-18 17:00:14 -0700

    webpage up and running the basics

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,4 +10,20 @@ class NotesController < ApplicationController
   def new #creates a new note in memory but NOT in the database
     @notes = Note.new
   end
+
+  def create
+    @notes = Note.new(notes_params)
+
+    if @notes.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def notes_params
+      params.require(:notes).permit(:title, :author, :body)
+    end
 end
